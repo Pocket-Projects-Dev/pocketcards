16219c8e4bfe81ab1946db2e8f2e54ca3c7fb011
Kaarthik Ashok <95279248+KatyGHub@users.noreply.github.com>
2026-03-05 08:20:44 +0000

    bugg

--- src/pages/NewEmi.tsx
+++ src/pages/NewEmi.tsx
@@ -1,4 +1,4 @@
-\import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { supabase } from "../lib/supabase";
 import { buildEmiSchedule } from "../lib/emi";
